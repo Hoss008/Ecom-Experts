b9bf6cefd8073cb0ae800d5c07eda06f79ebd49f
Hoss008 <43291152+Hoss008@users.noreply.github.com>
2026-06-17 16:00:21 +0300

    design system added

--- V1/src/components/ProductCard.jsx
+++ V1/src/components/ProductCard.jsx
@@ -3,14 +3,30 @@ import camera from "../assets/icon/24/cam/camera.svg";
 
 function ProductCard() {
   return (
-    <div className={styles.stepcontainer}>
-      <span className={styles.stepIndicator}>STEP 1 OF 4 </span>
+    <>
+      <div className={styles.bundleWrapper}>
+        <div className={styles.leftPanel}>
+          <div className={styles.stepcontainer}>
+            <span className={styles.stepIndicator}>STEP 1 OF 4</span>
 
-      <div className={styles.titleRow}>
-        <img src={camera} alt="camera icon" />
-        <h1 className={styles.title}>Choose your cameras</h1>
+            <div className={styles.titleRow}>
+              <div className={styles.titleLeft}>
+                <img src={camera} alt="camera icon" />
+                <h1 className={styles.title}>Choose your cameras</h1>
+              </div>
+
+              <span className={styles.selectedCount}>2 selected &#9652;</span>
+            </div>
+          </div>
+          <div />
+        </div>
+
+        {/* Your future right side */}
+        <aside className={styles.rightSidebar}>
+          {/* Review system goes here */}
+        </aside>
       </div>
-    </div>
+    </>
   );
 }
 
